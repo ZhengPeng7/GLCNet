b5616b8886cff09c5a67d466c91bbfefa838c446
ZhengPeng7 <zhengpeng0108@gmail.com>
2024-03-27 20:23:57 +0800

    Add time_now into log info to replace eta.

--- utils/utils.py
+++ utils/utils.py
@@ -127,10 +127,8 @@ class MetricLogger(object):
                 [
                     header,
                     "[{0" + space_fmt + "}/{1}]",
-                    "eta: {eta}",
+                    "time: {time_now}",
                     "{meters}",
-                    "time: {time}",
-                    "data: {data}",
                     "max mem: {memory:.0f}",
                 ]
             )
@@ -139,9 +137,8 @@ class MetricLogger(object):
                 [
                     header,
                     "[{0" + space_fmt + "}/{1}]",
-                    "eta: {eta}",
+                    "time: {time_now}",
                     "{meters}",
-                    "time: {time}",
                     "data: {data}",
                 ]
             )
@@ -151,17 +148,15 @@ class MetricLogger(object):
             yield obj
             iter_time.update(time.time() - end)
             if i % print_freq == 0 or i == len(iterable) - 1:
-                eta_seconds = iter_time.global_avg * (len(iterable) - i)
-                eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
+                time_now = str(datetime.datetime.now())
+                time_now = time_now.rstrip('.' + time_now.split('.')[-1])
                 if torch.cuda.is_available():
                     print(
                         log_msg.format(
                             i,
                             len(iterable),
-                            eta=eta_string,
+                            time_now=time_now,
                             meters=str(self),
-                            time=str(iter_time),
-                            data=str(data_time),
                             memory=torch.cuda.max_memory_allocated() / MB,
                         )
                     )
@@ -170,10 +165,8 @@ class MetricLogger(object):
                         log_msg.format(
                             i,
                             len(iterable),
-                            eta=eta_string,
+                            time_now=time_now,
                             meters=str(self),
-                            time=str(iter_time),
-                            data=str(data_time),
                         )
                     )
             i += 1
