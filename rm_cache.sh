5c42f7057d8e267b799eaf4a4b5225fcd303caab
ZhengPeng7 <zhengpeng0108@gmail.com>
2024-04-06 00:31:18 +0800

    Some updates on tool codes.

--- rm_cache.sh
+++ rm_cache.sh
@@ -1,6 +1,7 @@
 # Remove cache files
 rm -rf __pycache__
 rm -rf exp_*
+rm -rf vis/
 rm -rf data/
 rm *.out
 
--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,15 @@
 sleep ${2:-0}h
 
 # cuhk / prw / mvn
+dataset="cuhk"
+
+# Train
+CUDA_VISIBLE_DEVICES=${1:-0} python train.py --cfg configs/${dataset}.yaml
+
+# Test w/ CBGM
+CUDA_VISIBLE_DEVICES=${1:-0} python train.py --cfg ./exp_${dataset}/config.yaml --eval --ckpt ./exp_${dataset}/epoch_best.pth EVAL_USE_CBGM True
+
+# cuhk / prw / mvn
 dataset="prw"
 
 # Train
@@ -9,12 +18,3 @@ CUDA_VISIBLE_DEVICES=${1:-0} python train.py --cfg configs/${dataset}.yaml
 # Test w/ CBGM
 CUDA_VISIBLE_DEVICES=${1:-0} python train.py --cfg ./exp_${dataset}/config.yaml --eval --ckpt ./exp_${dataset}/epoch_best.pth EVAL_USE_CBGM True
 
-
-# cuhk / prw / mvn
-dataset="cuhk"
-
-# Train
-CUDA_VISIBLE_DEVICES=${1:-0} python train.py --cfg configs/${dataset}.yaml
-
-# Test w/ CBGM
-CUDA_VISIBLE_DEVICES=${1:-0} python train.py --cfg ./exp_${dataset}/config.yaml --eval --ckpt ./exp_${dataset}/epoch_best.pth EVAL_USE_CBGM True
