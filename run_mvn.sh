bf0db576e90361509b4b0fe65e8c0ac48ee3b76e
ZhengPeng7 <zhengpeng0108@gmail.com>
2024-04-18 12:00:30 +0800

    Reduce default training epochs on MovieNet-PS to 25.

--- run_mvn.sh
+++ run_mvn.sh
@@ -6,5 +6,5 @@ dataset="mvn"
 # Train
 CUDA_VISIBLE_DEVICES=${1:-0} python train.py --cfg configs/${dataset}.yaml
 
-# Test w/ CBGM
-CUDA_VISIBLE_DEVICES=${1:-0} python train.py --cfg ./exp_${dataset}/config.yaml --eval --ckpt ./exp_${dataset}/epoch_best.pth EVAL_USE_CBGM True
+# # Test w/ CBGM
+# CUDA_VISIBLE_DEVICES=${1:-0} python train.py --cfg ./exp_${dataset}/config.yaml --eval --ckpt ./exp_${dataset}/epoch_best.pth EVAL_USE_CBGM False
