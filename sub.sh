ebd04080584f36330d7c16555b2e9ae8d949308a
ZhengPeng7 <zhengpeng0108@gmail.com>
2024-07-08 02:52:58 +0800

    Model can take the pretraining weights of psvis and GLCNet on MovieNet-PS.

--- sub.sh
+++ sub.sh
@@ -7,7 +7,7 @@ script=${1:-"./run_all.sh"}
 
 devices=${2:-0}
 
-srun --nodes=1 -p vip_gpu_ailab -A ai4bio \
+sbatch --nodes=1 -p vip_gpu_ailab -A ai4bio \
 --ntasks-per-node=1 \
 --gres=gpu:$(($(echo ${devices%%,} | grep -o "," | wc -l)+1)) \
 --cpus-per-task=32 \
